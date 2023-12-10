83b544c888b5ac7896bdd25581ec1fefaf73e828
Meyn <loads@outlook.de>
2023-12-10 21:04:32 +0100

    Add Website Examples

--- UnitTest/DownloadTest.cs
+++ UnitTest/DownloadTest.cs
@@ -1,5 +1,6 @@
 using DownloadAssistant.Request;
 using Requests;
+using System.Diagnostics;
 using WebsiteScraper.Downloadable.Books;
 using WebsiteScraper.WebsiteUtilities;
 
@@ -8,7 +9,7 @@ namespace UnitTest
     [TestClass]
     public class DownloadTest
     {
-        private const string _pathToWebsite = @"D:\Bibliothek\Visual Studio\Librarys\WebsiteScraper\mangaread.org.wsf";
+        private string _pathToWebsite = @$"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName}\Websites\mangaread.org.wsf";
 
         private Website _mangaread = null!;
 
@@ -21,12 +22,29 @@ namespace UnitTest
         [TestMethod]
         public async Task SearchAsync()
         {
-            _mangaread.ItemsPerSearch = 20;
+            _mangaread.ItemsPerSearch = 10;
             await _mangaread.GetStatusTask();
-            Comic[] searchComics = await _mangaread.SearchAsync<Comic>("isekai");
-            Console.WriteLine("Comics found: " + searchComics.Length);
-            for (int i = 0; i < searchComics.Length; i++)
-                Console.WriteLine($"{i} | {searchComics[i].Title}");
+            SearchInfo searchinfo = new("isekai", _mangaread);
+            searchinfo.EnableAbleTags["action"].State = EnableAbleState.Enabled;
+            searchinfo.EnableAbleTags["comedy"].State = EnableAbleState.Enabled;
+            searchinfo.RadioTags["genres condition"].EnabledKey = "and";
+            List<Comic> searchComics = new();
+            searchComics.AddRange(await _mangaread.SearchAsync<Comic>(searchinfo));
+            searchComics.AddRange(await _mangaread.SearchNextAsync<Comic>());
+            Console.WriteLine("Comics found: " + searchComics.Count);
+            for (int i = 0; i < searchComics.Count; i++)
+            {
+                Console.WriteLine($"{i} | Title {searchComics[i].Title}");
+                Console.WriteLine($"{i} | Genres {string.Join("; ", searchComics[i].Genres)}");
+                Console.WriteLine($"{i} | Alternative titles{string.Join("; ", searchComics[i].AlternativeTitles)}");
+                Console.WriteLine($"{i} | Description: {searchComics[i].Description}");
+                Console.WriteLine($"{i} | Status: {searchComics[i].Status}");
+                Console.WriteLine($"{i} | Author: {searchComics[i].Author}");
+                Console.WriteLine($"{i} | Cover url: {searchComics[i].CoverUrl}");
+                Console.WriteLine($"{i} | Last updated: {searchComics[i].LastUpdated}");
+                Console.WriteLine($"{i} | Url: {searchComics[i].Url}");
+                Console.WriteLine("");
+            }
         }
 
         [TestMethod]
@@ -35,9 +53,25 @@ namespace UnitTest
             await _mangaread.GetStatusTask();
             Comic onePunch = new("https://www.mangaread.org/manga/one-punch-man-onepunchman/", "One Punch Man", _mangaread);
             await onePunch.UpdateAsync();
-            Console.WriteLine(onePunch.Title);
-            Console.WriteLine(onePunch.Chapter.Length + " Chapters");
-            Console.WriteLine("Description: " + onePunch.Description);
+            Console.WriteLine($"Title {onePunch.Title}");
+            Console.WriteLine($"Alternative titles: {string.Join("; ", onePunch.AlternativeTitles)}");
+            Console.WriteLine($"Description: {string.Join("; ", onePunch.Genres)}");
+            Console.WriteLine($"Status: {onePunch.Status}");
+            Console.WriteLine($"Author: {onePunch.Author}");
+            Console.WriteLine($"Chapter lenght: {onePunch.Chapter.Length}");
+            Console.WriteLine($"Last updated: {onePunch.LastUpdated}");
+            Console.WriteLine($"Cover url: {onePunch.CoverUrl}");
+            Console.WriteLine($"Url: {onePunch.Url}");
+        }
+
+        [TestMethod]
+        public async Task TestHomeAsync()
+        {
+            await _mangaread.GetStatusTask();
+            Comic[] newComic = await _mangaread.LoadNewAsync<Comic>();
+            Console.WriteLine($"Found {newComic.Length} new Comics");
+            Comic[] rComic = await _mangaread.LoadExtraAsync<Comic>("Recommended");
+            Console.WriteLine($"Found {rComic.Length} new Comics");
         }
 
         [TestMethod]
@@ -46,7 +80,7 @@ namespace UnitTest
             await _mangaread.GetStatusTask();
             Comic onePunch = new("https://www.mangaread.org/manga/one-punch-man-onepunchman/", "One Punch Man", _mangaread);
             await onePunch.UpdateAsync();
-            ProgressableContainer<LoadRequest> container = await onePunch.Chapter.First().DownloadAsync(@"D:\Bibliothek\Downloads\One Punch Man\1\");
+            ProgressableContainer<LoadRequest> container = await onePunch.Chapter.First().DownloadAsync(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\One Punch Man\1\");
             container.StateChanged += (s, e) => Console.WriteLine("State: " + e);
             await container.Task;
             Console.WriteLine("Downloaded");
